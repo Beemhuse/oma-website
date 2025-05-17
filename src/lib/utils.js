96e5b2363a0a000452dced7bfbef5e8710789dd3
Beemhuse <brightawah94@gmail.come>
2025-05-17 11:14:37 +0100

    added verification page

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -5,3 +5,8 @@ export function cn(...inputs) {
   return twMerge(clsx(inputs))
 }
 
+
+export const formatDate = (date) => {
+  const d = new Date(date);
+  return `${d.getDate().toString().padStart(2, '0')}/${(d.getMonth() + 1).toString().padStart(2, '0')}/${d.getFullYear()}`;
+};
